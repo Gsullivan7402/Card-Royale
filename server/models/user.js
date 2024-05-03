84c2cb3e3e3e46b58c40dbc0819d7f15b48cbfbf
Devin <devin.everitt96@gmail.com>
2024-05-02 20:50:35 -0400

    Better formatted the user stuff

--- server/models/user.js
+++ server/models/user.js
@@ -7,25 +7,24 @@ const profileSchema = new Schema({
     unique: true,
     trim: true,
   },
-  password: [
+  password:
     {
       type: String,
       required: true,
+      match: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
     },
-  ],
 });
 
 profileSchema.statics.isThisUsernameInUse = async function(username) {
     try {
-    const user = await this.findone({username})
-    if(user) return false
-    
-    return true;
-} catch (error) {
-    console.log(error.message)
-    return false
-}
-}
+        const user = await this.findOne({ username });
+        if (user) return true; // Username is already in use
+        return false; // Username is not in use
+    } catch (error) {
+        console.log(error.message);
+        return false; // Error occurred, treat as if the username is in use
+    }
+};
 
 const Profile = model('Profile', profileSchema);
 
