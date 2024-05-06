4e4f28083a1678511dca34aca007382fee113569
JohnChewning <johnathanchewning@gmail.com>
2024-05-06 19:50:56 -0400

    added logout button and deleted more files

---
+++ client/src/components/Logout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logout = () => {
+  return (
+    <button className="button p-1.5 m-3 bg-gray-200 text-bold border border-black rounded">Logout</button>
+  )
+}
+
+export default Logout;
