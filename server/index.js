1f4794f9ca8d40170e40c1fb25e54bf64d504201
Garrett Sullivan <gsullivan7402@gmail.com>
2024-05-06 18:52:16 -0400

    added code to routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 // Import express
 const express = require('express');
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+const authRoutes = require('../routes/authRoutes');
+const gameRoutes = require('../routes/gameRoutes');
+
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/game', gameRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the War Card Game Server!');
