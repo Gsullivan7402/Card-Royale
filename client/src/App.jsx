e9f554e00c2a00a120f5cdcce6d7c4d4482fc0a8
Garrett Sullivan <gsullivan7402@gmail.com>
2024-05-10 12:05:17 -0400

    added displayleaderboard code to componets. still need to display on page tho,

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import Login from './components/Login';
 import Homepage from './pages/HomePage';
 
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,6 +12,7 @@ function App() {
       <Header />
       <Login />
       <Homepage />
+      
 
     </div>
        
