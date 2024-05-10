9a189d94ed050d6fd77b2b12ab93737c43ffae59
Devin <devin.everitt96@gmail.com>
2024-05-09 20:28:21 -0400

    Still doesn't work but got some stuff fixed

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -4,16 +4,18 @@ const {
   getSingleUser,
   incrementVictories,
   login,
-} = require('../controllers/user-controller');
+} = require('../../controllers/user-controller');
+
+console.log("hello world");
 
 // import middleware
-const { authMiddleware } = require('../utils/auth');
+const { authMiddleware } = require('../../utils/auth');
 
 // put authMiddleware anywhere we need to send a token for verification of user
 router.route('/').post(createUser).put(authMiddleware, incrementVictories);
 
 router.route('/login').post(login);
 
-router.route('/me').get(authMiddleware, getSingleUser);
+router.route('/me').get(getSingleUser);
 
 module.exports = router;
---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const path = require('path');
+const apiRoutes = require('./api');
+
+console.log(`api: ${JSON.stringify(apiRoutes)}`)
+
+router.use('/api', apiRoutes);
+
+// serve up react front-end in production
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  });
+
+module.exports = router;
