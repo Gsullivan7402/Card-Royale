0ca954dbd50407037e37ff13a3732976cf567f97
Garrett Sullivan <gsullivan7402@gmail.com>
2024-05-11 19:20:40 -0400

    leaderboard toggle displayed but not working

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,29 +2,34 @@ import React from 'react';
 import Button from './Button';
 import Auth from '../utils/auth';
 import Login from './Login';
-
+import toggleLeaderboard from './displayleaderboard'; // Ensure the correct file name and export
 
 const Header = () => {
   return (
-    <div className='bg-green-800 relative' >
+    <div className='bg-green-800 relative'>
       <h1 className='font-sans text-4xl text-center text-white'>Card Royale</h1>
-      {Auth.loggedIn() ? (
-
+      <div style={{ position: 'absolute', right: '50px', top: '10px' }}>
+        {Auth.loggedIn() ? (
+          <Button
+            onClick={Auth.logout}
+            className="w-40 ml-6 py-2 px-4 rounded bg-black text-white font-bold"
+          >
+            Log Out
+          </Button>
+        ) : (
+          <Login />
+        )}
+      </div>
+      <div style={{ position: 'absolute', left: '30px', top: '10px' }}>
         <Button
-          onClick={Auth.logout}
-          className="w-40 ml-6 py-2 px-4 rounded bg-black text-white font-bold flex justify-center absolute right-4 top-4"
+          onClick={toggleLeaderboard}
+          className="w-40 ml-6 py-2 px-4 rounded bg-black text-white font-bold"
         >
-          Log Out
+          Toggle Leaderboard
         </Button>
-
-      ) : (
-        <Login />
-      )
-      }
+      </div>
     </div>
-
-
-  )
+  );
 }
 
 export default Header;
