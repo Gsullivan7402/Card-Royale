79e852e7766f39b4972f7240f13575f852b42519
JohnChewning <johnathanchewning@gmail.com>
2024-05-13 16:12:10 -0400

    added event listener to prevent scrolling and nav bar for mobile format

--- client/src/components/displayleaderboard.jsx
+++ client/src/components/displayleaderboard.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import List from './Leaderboard/list'; // Ensure this is the correct path and correct capitalization
+import React, { useState } from "react";
+import List from "./Leaderboard/list"; // Ensure this is the correct path and correct capitalization
 
 function ToggleLeaderboard() {
   const [isVisible, setIsVisible] = useState(false);
@@ -8,9 +8,9 @@ function ToggleLeaderboard() {
     <div>
       <button
         onClick={() => setIsVisible(!isVisible)}
-        className="relative top-[-250px] bg-transparent text-white font-semibold py-2 px-4 border border-black rounded hover:bg-black hover:text-green-200 active:bg-green-800 active:text-white active:border-green-800"
+        className="absolute top-8 left-4 bg-green-900 text-white font-semibold py-2 px-4 border border-black rounded hover:bg-black hover:text-green-200 active:bg-green-800 active:text-white active:border-green-800 cursor-pointer hover:scale-110 ease-in duration-200 lg:block hidden"
       >
-        {isVisible ? 'Hide Leaderboard' : 'Show Leaderboard'}
+        {isVisible ? "Hide Leaderboard" : "Show Leaderboard"}
       </button>
       {isVisible && <LeaderboardView />}
     </div>
@@ -19,15 +19,16 @@ function ToggleLeaderboard() {
 
 function LeaderboardView() {
   const users = [
-    { userID: '1', displayName: 'Garrett ', wins: 300 },
-    { userID: '2', displayName: 'John ', wins: 200 },
-    { userID: '3', displayName: 'Devin ', wins: 100 },
+    { userID: "1", displayName: "Garrett ", wins: 300 },
+    { userID: "2", displayName: "John ", wins: 200 },
+    { userID: "3", displayName: "Devin ", wins: 100 },
   ];
-
 
   return (
     <div className="w-full max-w-2xl mx-auto mt-8 bg-green-100 shadow-xl rounded-lg p-4">
-      <h1 className="text-lg font-bold text-green-800 text-center">Leaderboard of Champions!</h1>
+      <h1 className="text-lg font-bold text-green-800 text-center">
+        Leaderboard of Champions!
+      </h1>
       <div className="overflow-auto">
         <List users={users} />
       </div>
